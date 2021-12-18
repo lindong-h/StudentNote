b4ff74ac09665d0ab80c227b7098873017278e5c
lindong-h <79568927+lindong-h@users.noreply.github.com>
2021-12-18 18:56:00 +0800

    master

--- 邻接表.cpp
+++ 邻接表.cpp
@@ -150,7 +150,28 @@ void InverseList(AdjList A , AdjList B)
 // 判断路径 DFS
 int DFS(int i , int j) // 判断 i 到 j 之间是否存在路径 memset(st , 0 , sizeof st);
 {   
-    
+    int stack[N] , tt = -1;
+    st[i] = 1;
+    stack[++tt] = i;
+    int flag = 0;
+    while(tt != -1 || flag == 0)
+    {
+        i = stack[tt];
+        p = adjlist[i].firstEdge;
+        while(p && flag == 0)
+        {
+            auto t = p -> adjvex;
+            if(t == j) flag = 1;
+            else if(st[t] == 0)
+            {
+                st[t] = 1;
+                stack[++tt] = t;
+            }
+            else p = p -> next;
+        }
+        if(!p) tt --;
+    }
+    return flag;
 }
 
 // 判断路径 BFS
@@ -166,7 +187,7 @@ int BFS(int i , int j) // 判断 i 到 j 之间是否存在路径 memset(st , 0 
         i = q[++hh];
         auto p = adjlist[i].firstEdge;
 
-        while(p)
+        while(p && flag == 0)
         {
             auto t = p -> adjvex;
             if(t == j) flag = 1;
@@ -178,4 +199,5 @@ int BFS(int i , int j) // 判断 i 到 j 之间是否存在路径 memset(st , 0 
             else p = p -> next;
         }
     }
+    return flag;
 }
